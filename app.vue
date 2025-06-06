c144532ba516efcae2cc1e9e3b2e256592a9b76b
iagugunava <iagugunava05@gmail.com>
2025-06-06 16:11:33 +0400

    created temporary coming soon page

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
-
+// change name to default once content is ready, delete layouts/coming-soon.vue and pages/coming-soon.vue files and change middleware logic
 
 </script>
 
 <template>
   <div>
-    <NuxtLayout name="default">
+    <NuxtLayout name="coming-soon">
       <NuxtPage/>
     </NuxtLayout>
     <GlobalProvider/>
---
+++ pages/coming-soon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full h-[100dvh] flex flex-col gap-5 justify-center items-center">
+        <h1 class="text-4xl text-primary font-bold text-center">
+            ვებსაიტი დეველოპმენტის რეჟიმშია
+        </h1>
+        <p class="text-center text-secondary font-semibold text-xl">We’re getting things ready. Stay tuned!</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
